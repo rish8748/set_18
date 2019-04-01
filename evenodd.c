f7c3b0bb64dbf1e6e303a2969f89597b996d5769
rish8748 <44296379+rish8748@users.noreply.github.com>
2019-04-01 09:21:38 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main()
+{
+   int n;
+   scanf("%d",&n);
+   if(n>=1 && n<=100000)
+   {
+   if ( n%2==0)
+      printf("Even");
+   else 
+      printf("Odd");
+   }
+ else
+ printf("Invalid");
+   return 0;
+}
